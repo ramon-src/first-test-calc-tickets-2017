cd7bee84edbb2e27d31702b41ae2c01031fafd63
ramon183 <ramonroc@gmail.com>
2017-02-07 22:17:37 -0200

    Add all tests from Discount Child and Student

--- tests/Discounter/DiscountChildTest.java
+++ tests/Discounter/DiscountChildTest.java
@@ -11,19 +11,47 @@ public class DiscountChildTest {
     Child child;
     DiscountChild discountChild;
     Ticket ticket;
-    double ticketPrice;
 
     @Before
     public void setUp() {
         child = new Child();
         discountChild = new DiscountChild();
         ticket = new Ticket(child);
-        ticketPrice = ticket.getPriceByPerson();
     }
 
+    @Test
+    public void getChildTicketPrice5_5AtSunday() {
+        double totalPriceResulted = discountChild.calculate(ticket, 1);
+        Assert.assertEquals(5.5, totalPriceResulted, 0.1);
+    }
+    @Test
+    public void getChildTicketPrice4_95AtMonday() {
+        double totalPriceResulted = discountChild.calculate(ticket, 2);
+        Assert.assertEquals(4.95, totalPriceResulted, 0.1);
+    }
+    @Test
+    public void getChildTicketPrice4_67AtTuesday() {
+        double totalPriceResulted = discountChild.calculate(ticket, 3);
+        Assert.assertEquals(4.67, totalPriceResulted, 0.1);
+    }
+    @Test
+    public void getChildTicketPrice3_85AtWednesday() {
+        double totalPriceResulted = discountChild.calculate(ticket, 4);
+        Assert.assertEquals(3.85, totalPriceResulted, 0.1);
+    }
+    @Test
+    public void getChildTicketPrice5_5AtThursday() {
+        double totalPriceResulted = discountChild.calculate(ticket, 5);
+        Assert.assertEquals(5.5, totalPriceResulted, 0.1);
+    }
     @Test
     public void getChildTicketPrice4_89AtFriday() {
         double totalPriceResulted = discountChild.calculate(ticket, 6);
         Assert.assertEquals(4.89, totalPriceResulted, 0.1);
+    }
+    @Test
+    public void getChildTicketPrice5_5AtSaturday() {
+        double totalPriceResulted = discountChild.calculate(ticket, 7);
+        Assert.assertEquals(5.5, totalPriceResulted, 0.1);
     }
 }
--- tests/Discounter/DiscountStudentTest.java
+++ tests/Discounter/DiscountStudentTest.java
@@ -20,15 +20,51 @@ public class DiscountStudentTest {
     }
 
     @Test
+    public void getStudentTicketPrice8_0AtSunday() {
+        double totalPriceResulted = discountStudent.calculate(ticket, 1);
+        Assert.assertEquals(8.0, totalPriceResulted, 0.1);
+    }
+
+    @Test
     public void getStudentTicketPrice7_20AtMonday() {
         double totalPriceResulted = discountStudent.calculate(ticket, 2);
         Assert.assertEquals(7.2, totalPriceResulted, 0.1);
     }
 
+    @Test
+    public void getStudentTicketPrice7_60AtTuesday() {
+        double totalPriceResulted = discountStudent.calculate(ticket, 3);
+        Assert.assertEquals(7.6, totalPriceResulted, 0.1);
+    }
+
+    @Test
+    public void getStudentTicketPrice4_0AtWednesday() {
+        double totalPriceResulted = discountStudent.calculate(ticket, 4);
+        Assert.assertEquals(4.0, totalPriceResulted, 0.1);
+    }
+
+    @Test
+    public void getStudentTicketPrice5_6AtThursday() {
+        double totalPriceResulted = discountStudent.calculate(ticket, 5);
+        Assert.assertEquals(5.6, totalPriceResulted, 0.1);
+    }
+
+    @Test
+    public void getStudentTicketPrice8_0AtFriday() {
+        double totalPriceResulted = discountStudent.calculate(ticket, 6);
+        Assert.assertEquals(8.0, totalPriceResulted, 0.1);
+    }
+
+    @Test
+    public void getStudentTicketPrice8_0AtSaturday() {
+        double totalPriceResulted = discountStudent.calculate(ticket, 7);
+        Assert.assertEquals(8.0, totalPriceResulted, 0.1);
+    }
 
     @Test
     public void getStudentTicketPrice5_20WithStudentCard() {
         Student studentWithCard = new Student();
+
         studentWithCard.setCardToTrue();
         ticket = new Ticket(studentWithCard);
         double totalPriceResulted = discountStudent.calculate(ticket, 2);
